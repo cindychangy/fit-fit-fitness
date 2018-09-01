7589f21239fcec2f00a9c2f9eccbe1fad51ab7a2
cindychangy <cindyisachang@gmail.com>
2018-09-01 17:13:17 -0500

    re-factored code

--- src/app.js
+++ src/app.js
@@ -1,49 +1,55 @@
 
 const videos = [
-  {
-    title: 'example site one',
-    url: 'www.google.com'
-  },
-  {
-    title: 'example site two',
-    url: 'www.yahoo.com'
-  },
-  {
-    title: 'example site three',
-    url: 'www.time.com'
-  }
+	'https://www.youtube.com/embed/18hPypKsQ8w',
+	'https://www.youtube.com/embed/VUipWn2gf1M',
+	'https://www.youtube.com/embed/STN3KpGIxcY'
 ];
 
-const videoUrl = document.getElementById('videoUrl');
-const showInput = document.getElementById('showInput');
+const showInputBtn = document.getElementById('showInput');
 const submitBtn = document.getElementById('submit');
 const pickBtn = document.getElementById('pick');
-const inputElements = document.getElementsByClassName('nav__input')[0];
 const displayVideo = document.getElementById('display-video');
+const modal = document.getElementsByClassName('modal')[0];
 
-//pick + display random video
-pickBtn.addEventListener('click', () => {
+//get a random video
+const getVideo = () => {
   let picked = videos[Math.floor(Math.random() * videos.length)];
-  displayVideo.innerHTML = picked;
-  console.log(picked);
-});
+  const videoContainer = document.getElementsByClassName('modal__video-container')[0];
 
-//play video in fullscreen
+  //show video
+  modal.classList.toggle('show-modal');
+  videoContainer.innerHTML = `<iframe id="videoFrame" width="853" height="480" src="${picked}" frameborder="0" allowfullscreen"></iframe>`;
+}
 
-//show input components
-showInput.addEventListener('click', () => {
+//close video
+const closeBtn = document.getElementById('close');
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.toggle('show-modal');
+})
+
+//show the video
+pickBtn.addEventListener('click', getVideo);
+
+//show add video component
+showInputBtn.addEventListener('click', () => {
+  const inputElements = document.getElementsByClassName('nav__input')[0];
   inputElements.classList.toggle('visible');
+
+  // this.classList.toggle('rotate');
 });
 
 //add video functionality
-
 const addNewVideo = () => {
+  const videoUrl = document.getElementById('videoUrl');
   videos.push(videoUrl.value);
   videoUrl.value = '';
 }
 
-submitBtn.addEventListener('click', addNewVideo)
+submitBtn.addEventListener('click', addNewVideo);
 
 
-//be able to delete videos from array
+////////need to do
+//set up db
+//change plus to x to remove videos from db
 
